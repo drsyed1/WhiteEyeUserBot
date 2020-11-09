238791873373a56b2811c541fd814fcb488b3439
mrdayamzaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-11-09 11:26:15 +0530

    Update alive.py

--- WhiteEyeUserBot/modules/alive.py
+++ WhiteEyeUserBot/modules/alive.py
@@ -6,7 +6,6 @@ from uniborg.util import WhiteEye_on_cmd, sudo_cmd
 
 from WhiteEyeUserBot import ALIVE_NAME, Lastupdate
 from WhiteEyeUserBot.Configs import Config
-from WhiteEyeUserBot.modules import currentversion
 
 
 # Functions
