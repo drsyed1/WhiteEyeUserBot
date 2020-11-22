532d1a353b776b2258f3dd9132644d6495d868a6
mrdayamzaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-11-22 14:08:51 +0530

    Update __main__.py

--- WhiteEyeUserBot/__main__.py
+++ WhiteEyeUserBot/__main__.py
@@ -5,6 +5,7 @@ import telethon.utils
 from telethon import TelegramClient
 
 from WhiteEyeUserBot import bot
+from WhiteEyeUserBot.Configs import Config
 from WhiteEyeUserBot.utils import load_module, start_assistant
 from var import Var
 
@@ -20,15 +21,10 @@ if len(argv) not in (1, 3, 4):
 else:
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        print("Initiating Inline Bot")
-        # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
             "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        print("Initialisation finished with no errors")
-        print("Starting To Install Inline In Bot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("Startup Completed")
     else:
         bot.start()
 
@@ -42,19 +38,22 @@ for name in files:
         path1 = Path(f.name)
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
-# Done.
-path = "WhiteEyeUserBot/modules/assistant/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        start_assistant(shortname.replace(".py", ""))
 
-print("WhiteEye And Assistant Bot Have Been Installed Successfully !")
-print("You Can Visit @WhiteEyeOT For Any Support Or Doubts")
+if Config.ENABLE_ASSISTANTBOT == "ENABLE":
+    path = "WhiteEyeUserBot/modules/assistant/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            start_assistant(shortname.replace(".py", ""))
+    print("WhiteEye And Assistant Bot Have Been Installed Successfully !")
+else:
+    print("WhiteEye Has Been Installed Sucessfully !")
+    print("You Can Visit @WhiteEyeOT For Any Support Or Doubts")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
+
